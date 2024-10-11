da1e95147fb5b2e436fdc74463fe3d99654a5e4e
dldlscjf123 <dldlscjf1231@naver.com>
2024-10-11 11:41:37 +0900

    단어 뒤집기

--- list/changeString.java
+++ list/changeString.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class changeString {
     /*
     단어 뒤집기
+    문자열 추가 수정-- > 반복문 안에서는 StringBuilder사용생각하기!!!
     첫째 줄에 테스트 케이스의 개수 T가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있으며,
     문장이 하나 주어진다. 단어의 길이는 최대 20, 문장의 길이는 최대 1000이다.
     단어와 단어 사이에는 공백이 하나 있다.
