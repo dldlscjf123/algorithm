e2ec3ca260266b89568e22eec0531cebbfe07fd6
dldlscjf123 <dldlscjf1231@naver.com>
2024-11-20 15:22:47 +0900

    [silver2] 에디터

--- struct/Editor.java
+++ struct/Editor.java
@@ -8,7 +8,7 @@ import java.io.OutputStreamWriter;
 import java.util.Stack;
 
 public class Editor {
-
+    //[실버2]에디터
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
